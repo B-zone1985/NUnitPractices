67e96f8f0ecdfed808b1ca48bba867f341480094
Oleg <oleg.kurkov.85@gmail.com>
2023-01-10 15:53:00 +0300

    Добавьте файлы проекта.

---
+++ NUnitPractices.Tests/CalculatorTests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitPractices.Tests
+{
+    internal class CalculatorTests
+    {
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(100, 10) == 110);
+        }
+
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(100, 10) == 90);
+        }
+
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(100, 10) == 1000);
+        }
+
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            Assert.That(calculator.Division(100, 10) == 10);
+        }
+    }
+}
